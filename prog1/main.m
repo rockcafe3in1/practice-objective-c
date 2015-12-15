a7f7ed60fd38df99ecc7d051e40815709f2e5427
rockcafe <Liang7Zhang@gmail.com>
2015-12-15 22:55:54 +0800

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by LIANG ZHANG on 12/15/15.
+//  Copyright © 2015 liang1zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
