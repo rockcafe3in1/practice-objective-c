1bc10c6bdb9738879832fed550661d38075bd353
liang1zhang <liang1zhang@yahoo.com>
2015-12-16 00:11:52 +0800

    Practice to use dev branch
    Instead of modifing your master branch, you
    could change your code in dev branch and then
    get your changes merged to master branch.
    master branch can be an common place, where
    you can upload code to github

--- prog1/main.m
+++ prog1/main.m
@@ -16,6 +16,9 @@ int main (int argc,const char * argv[])
         NSLog (@"Once again, this is my first Objective-C program...");
         NSLog (@"Pay attention on my new git user and email...");
         NSLog (@"Tell me how to upload my first program to github");
+        NSLog (@"Practice to checkout an new branch, which is called dev02");
+        NSLog (@"Any code changes should exsit in dev02 branch firstly, then");
+        NSLog (@"master branch receive commit branch devloping branch, like dev02");
     }
     return 0;
 }
