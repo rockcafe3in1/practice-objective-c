06f2078edfbeabfdf2002fe6aaea7360836a6ca1
liang1zhang <liang1zhang@yahoo.com>
2015-12-16 22:43:22 +0800

    Format code and compile from commandline

--- prog1/main.m
+++ prog1/main.m
@@ -8,18 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-int main (int argc,const char * argv[])
-{
-    @autoreleasepool {
-        NSLog (@"Programming Is Fun...");
-        NSLog (@"Hey! This is Liang speaking...");
-        NSLog (@"Once again, this is my first Objective-C program...");
-        NSLog (@"Pay attention on my new git user and email...");
-        NSLog (@"Tell me how to upload my first program to github");
-        NSLog (@"Practice to checkout an new branch, which is called dev02");
-        NSLog (@"Any code changes should exsit in dev02 branch firstly, then");
-        NSLog (@"master branch receive all commits from dev branch, like dev02");
-        NSLog (@"This work flow can be a good chooise!!!");
-    }
+int main (int argc,const char * argv[]){
+	@autoreleasepool {
+		NSLog (@"Programming Is Fun...");
+		NSLog (@"Hey! This is Liang speaking...");
+		NSLog (@"Once again, this is my first Objective-C program...");
+		NSLog (@"Pay attention on my new git user and email...");
+		NSLog (@"Tell me how to upload my first program to github");
+		NSLog (@"Practice to checkout an new branch, which is called dev02");
+		NSLog (@"Any code changes should exsit in dev02 branch firstly, then");
+		NSLog (@"master branch receive all commits from dev branch, like dev02");
+		NSLog (@"This work flow can be a good chooise!!!");
+		NSLog (@"It's a whole new world...");
+        NSLog (@"You could compile your objective-c program from your commandline");
+        NSLog (@"try this command: $ clang -fobjc-arc main.m -o prog1");
+	}
     return 0;
 }
