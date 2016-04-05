1f2114b7279cd0bde931165e39ee84979b3b80cf
liang1zhang <liang1zhang@yahoo.com>
2016-04-05 23:47:56 +0800

    Add demonstration of mutable string usage

--- chapter15-nsstring-usage.m
+++ chapter15-nsstring-usage.m
@@ -8,14 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, char* argv[])
+int main_string_basic(int argc, char* argv[])
 {
     @autoreleasepool {
+        
+        /* Branch A: immutable string case */
         
         /* Print a simple constant string */
         NSString *str = @"Programming is fun";
         NSLog (@"%@", str);
-        
         
         /* Use %@ to display integer object */
         NSNumber *intNumber = @100;
@@ -29,7 +30,35 @@ int main(int argc, char* argv[])
         // NSLog (@"%c", charValue);
         // Instead of, convert char object to char
         NSLog (@"%c", [charValue charValue]);
-
+        
+        /* Could i use NSString's subclass NSConstantString */
+        /* Compiler show me that incompatible pointer with NSString Object */
+        NSConstantString *constString = @"Hey! Liang, have a nice day";
+        NSLog (@"%@", constString);
+        
+        /* Talk about %@ specifier
+         * The NSlog format characters %@ can be used to display not just NSString objects
+         * but other objects as well.
+         */
+        NSNumber *intNumber2 = @100;
+        NSLog (@"%@", intNumber2);
+        
+        /* Override description method to print what you want */
+        /* - (NSString*) description
+         *   {
+         *       NSString stringWithFormat: @"%i/%i"
+         *   }
+         */
+        // YourClass *yourClass = @"your-class";
+        // NSLog (@"%@", yourClass);
+        
+        /* The description method allows you to display your objects in a meaningful way */
+        // sum = [f1 add: f2]
+        // NSLog (@"%@, %@, sum is %@", f1, f2, sum);
+        
+        /* Branch B: Mutable string case */
+        
+        
     }
     return 0;
 }
---
+++ prog1/chapter15-mutable-string.m
@@ -0,0 +1,61 @@
+//
+//  chapter15-mutable-string.m
+//  prog1
+//
+//  Created by LIANG ZHANG on 4/5/16.
+//  Copyright © 2016 liang1zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/* We will show you some usage tip about mutable string */
+int main(int argc, char* argv[])
+{
+    @autoreleasepool {
+        
+        
+        NSString *str1 = @"This is string A";
+        NSMutableString *mstr;
+        NSRange substr; /* typedef structure */
+        
+        
+        /* Create mutable string from nonmutable */
+        mstr = [NSMutableString stringWithString: str1];
+        NSLog (@"%@", mstr);
+        
+        /* Insert characters */
+        [mstr insertString: @" mutable" atIndex:7];
+        NSLog (@"%@", mstr);
+        
+        /* Concatenation if insert at end */
+        [mstr insertString: @" and string B" atIndex: [mstr length]];
+        NSLog (@"%@", mstr);
+        
+        /* Or can use appendString directly */
+        [mstr appendString: @" and string C"];
+        NSLog (@"%@", mstr);
+        
+        /* Delete substring based on rang */
+        [mstr deleteCharactersInRange: NSMakeRange(16, 13)];
+        NSLog (@"%@", mstr);
+        
+        /* Find range first and then use it for deletion */
+        substr = [mstr rangeOfString: @"string B and " ];
+        if (substr.location != NSNotFound) {
+            [mstr deleteCharactersInRange: substr];
+            NSLog (@"%@", mstr);
+        }
+        
+        /* Replace mutable string with new one */
+        [mstr setString: @"This is string A"];
+        NSLog (@"%@", mstr);
+        
+        /* Replace a range of chars with another */
+        [mstr replaceCharactersInRange: NSMakeRange(8, 8) withString: @"a mutable string"];
+        NSLog (@"%@", mstr);
+        
+    }
+    
+    return 0;
+}
